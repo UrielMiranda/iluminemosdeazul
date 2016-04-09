f6e03a5ff96327cd6393615508d702b4325a98fe
Uriel Miranda <urielnovoa@gmail.com>
2016-04-08 23:56:38 -0500

    Merge Errors

--- azul/contacto/views.py
+++ azul/contacto/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import ContactForm
-<<<<<<< HEAD
-from django.core.mail import EmailMessage
-# Create your views here.
-
-=======
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
-
-# Create your views here.
-
 from django.core.mail import EmailMessage
 
->>>>>>> ac629cab34617b86d75969935ccca89fb8c5c131
 
 
 class Contacto(View):
@@ -45,9 +36,4 @@ class Contacto(View):
 			print('Exito')
 		except:
 			print('Error')
-
-<<<<<<< HEAD
-
-=======
->>>>>>> ac629cab34617b86d75969935ccca89fb8c5c131
 		return redirect('contacto:contactoapp')
