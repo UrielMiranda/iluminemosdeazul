716795427098e6d4fd89fa4c7521d1884287f9ed
joseguerreroc <neuroppgc@gmail.com>
2016-04-08 21:59:32 -0500

    Contact form a email ya funcionando sin estilos y enlaces de nav y footer

--- azul/contacto/views.py
+++ azul/contacto/views.py
@@ -1,43 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-
 from .forms import ContactForm
-
-from django.template.loader import get_template
 from django.core.mail import EmailMessage
-from django.template import Context
 
-# Create your views here.
-
-
-	# def contact(request):
-	# 	form_class = ContactForm
-
-	# 	if request.method == 'POST':
-	# 		form = form_class(data=request.POST)
-	# 		if form.is_valid():
-	# 			nombre_contacto = request.POST.get('nombre_contacto', '')
-	# 			mail_contacto = request.POST.get('mail_contacto', '')
-	# 			contenido = request.POST.get('contenido', '')
-
-	# 			template= get_template('contact_template.txt')
-	# 			context= Context({
-	# 				'nombre_contacto': nombre_contacto,
-	# 				'mail_contacto': mail_contacto,
-	# 				'contenido': contenido,
-	# 				})
-	# 			content = template.render(context)
-
-	# 			email = EmailMessage("Alquien quiere contactarnos", content,
-	# 				"Iluminemos de Azul" + '', ['gaya@iluminemosdeazul.org'],
-	# 				headers = {'Reply-to': mail_contacto})
-	# 			email.send()
-	# 			return redirect('contact')
-
-	# 	return render(request, 'contacto.html', 
-	# 		{
-	# 		'form': form_class,
-	# 		})
 
 class Contacto(View):
 	def get(self,request):
@@ -49,15 +14,31 @@ class Contacto(View):
 	def post(self,request):
 		form=ContactForm(request.POST)
 		if form.is_valid():
-			pass
-		#Aqui envia email
-		email = EmailMessage('Alguien quiere contactarnos', 'contenido template aqui',
-			'mail_contacto', ['iluminemosazul@gmail.com'], reply_to= ['madelin.pjm@aoutlook.com'],
-			headers={'Mensaje': 'foo'} )
+			nombre_contacto= form.cleaned_data['nombre_contacto']
+			mail_contacto= form.cleaned_data['mail_contacto']
+			contenido= form.cleaned_data['contenido']
+		content= """
+		Hay alguien interesado en Iluminemos de Azul
 
-		email.send()
-		
+		Nombre: %s
+		Mail:  %s 
+		Mensaje: %s"""	% (nombre_contacto, mail_contacto, contenido) 
+		email = EmailMessage('Alguien quiere contactarnos', content,
+			mail_contacto, ['iluminemosazul@gmail.com'], reply_to= [mail_contacto],
+			headers={'Mensaje': 'foo'} )
+		try:
+			email.send()
+			print('Exito')
+		except:
+			print('Error')
+
 		return redirect('contacto:contactoapp')
+
+		
+	
+
+
+
 
 
 
