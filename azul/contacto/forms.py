b4607b4ad7907d487452d575f9a1a4da6be26b5c
Uriel Miranda <urielnovoa@gmail.com>
2016-04-09 04:12:55 -0500

    contacto update style

--- azul/contacto/forms.py
+++ azul/contacto/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class ContactForm(forms.Form):
 	nombre_contacto= forms.CharField(required=True,label="Dinos tu nombre")
 	mail_contacto= forms.EmailField(required=True, label="Pon tu email")
-	contenido= forms.CharField(required=True, label="Escribenos tus comentarios", widget=forms.Textarea)
-	
+	contenido= forms.CharField(required=True, label="Escribenos tus comentarios", widget=forms.Textarea(attrs={'class': 'form-blue'}))
+
 	# def __init__(self, *args, **kwargs):
 	# 	super(ContactForm, self).__init__(*args, **kwargs)
 	# 	self.fields['nombre_contacto'].label = "Dinos tu nombre:"
