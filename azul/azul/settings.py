c016338b80952862e603c8dedd67820e2a2479e8
Uriel Miranda <urielnovoa@gmail.com>
2016-04-20 12:49:14 -0500

    Fix static path

--- azul/azul/settings.py
+++ azul/azul/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 )
 
 STATIC_URL = '/static/'
